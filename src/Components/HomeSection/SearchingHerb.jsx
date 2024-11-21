280d9b260a33d061418c2f50300f487ef2251d57
shubhamsheoran17 <shubhamsheoran22@gmail.com>
2024-11-21 11:23:15 +0530

    fix

--- src/Components/HomeSection/SearchingHerb.jsx
+++ src/Components/HomeSection/SearchingHerb.jsx
@@ -7,7 +7,7 @@ function SearchingHerb(){
             <div className="flex justify-center items-center p-10 pt-24">
                 <p className="font-medium text-center text-xl p-5">  Ayurveda is also called the “science of longevity” because it offers a complete system to live a long healthy life. It offers programs to rejuvenate the body through diet and nutrition. It offers treatment methods to cure many common diseases such as food allergies, which have few modern treatments.</p>
             </div>
-            <div className="pt-24 ">
+            <div className="pt-24 flex justify-center items-center">
                 <h1 className="font-extrabold italic text-3xl pb-10">Some amazing herb</h1>
             </div>
             <ImageCarousel />
